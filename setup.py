49909345d787294d36f0246bcb3556f986263985
Adam Tauber <asciimoo@gmail.com>
2017-09-08 21:30:45 +0200

    [ver] version 0.10.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exrex',
-    version='0.10.4',
+    version='0.10.5',
     author='Adam Tauber',
     author_email='asciimoo@gmail.com',
     description='Irregular methods for regular expressions',
