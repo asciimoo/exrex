f4a64a49dccbdaf5c6849b8732c8819fada64b73
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-05-11 20:51:38 +0530

    Improved setup.py
    1. Separated package meta-data from setup to enable easy-edits.
    2. fixed support for long-description on PyPi
    3. added upload command
    4. added support for "setup.py upload" command

--- setup.py
+++ setup.py
@@ -1,18 +1,102 @@
-from setuptools import setup, find_packages
+import io
+import os
+import sys
+from shutil import rmtree
 
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'exrex'
+DESCRIPTION = 'Irregular methods for regular expressions'
+URL = 'https://github.com/asciimoo/exrex'
+EMAIL = 'asciimoo@gmail.co'
+AUTHOR = 'Adam Tauber'
+VERSION = '0.10.6'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # nothing in-here
+]
+
+# What packages are optional?
+EXTRAS = {
+    # nothing in-here
+}
+
+# ------------------------------------------------
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Setup:
 setup(
-    name='exrex',
-    version='0.10.5',
-    author='Adam Tauber',
-    author_email='asciimoo@gmail.com',
-    description='Irregular methods for regular expressions',
-    license='AGPLv3+',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
     keywords="regexp generators string generation regex simplification",
-    url='https://github.com/asciimoo/exrex',
-    scripts=['exrex.py'],
-    py_modules=['exrex'],
-    packages=find_packages(),
-    install_requires=[],
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='AGPLv3+',
     download_url='https://github.com/asciimoo/exrex/tarball/master',
     entry_points={
         "console_scripts": ["exrex=exrex:__main__"]
@@ -31,5 +115,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ]
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
